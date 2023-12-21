0bc0b7779b87d3dae27ecd674bb4fb693a303b80
kenwaysharma <kenwaysharma@gmail.com>
2023-12-21 19:21:44 +0530

    Added blog page

--- src/routes.js
+++ src/routes.js
@@ -47,6 +47,7 @@ import AboutUs from "layouts/pages/landing-pages/about-us";
 import ContactUs from "layouts/pages/landing-pages/contact-us";
 import Author from "layouts/pages/landing-pages/author";
 import SignIn from "layouts/pages/authentication/sign-in";
+import Blog from "layouts/pages/landing-pages/blog";
 
 // Sections
 import PageHeaders from "layouts/sections/page-sections/page-headers";
@@ -92,6 +93,11 @@ const routes = [
             name: "author",
             route: "/pages/landing-pages/author",
             component: <Author />,
+          },
+          {
+            name: "blog",
+            route: "/pages/landing-pages/blog",
+            component: <Blog />,
           },
         ],
       },
